9e84c6cfd1044403cef99058f1f0ef8757f50cfb
MagicianFrog <khang.na2005@gmail.com>
2024-05-05 23:07:16 +0700

    Add game textures

---
+++ main/assets.cpp
@@ -0,0 +1,76 @@
+#include "assets.hpp"
+using namespace std;
+
+vector <SDL_Texture*> backgroundTextures(2, nullptr);
+SDL_Texture *carTexture = nullptr;
+SDL_Texture *carInvisibleTexture = nullptr;
+SDL_Texture *obstacleSpriteTexture = nullptr;
+SDL_Texture *obstacleCrashedSpriteTexture = nullptr;
+SDL_Texture *coinSprite = nullptr;
+SDL_Texture *goldenFontTexture = nullptr;
+SDL_Texture *blueFontTexture = nullptr;
+SDL_Texture *whiteFontTexture = nullptr;
+SDL_Texture *metalFontTexture = nullptr;
+SDL_Texture *plainWhiteFontTexture = nullptr;
+SDL_Texture *plainBlackFontTexture = nullptr;
+SDL_Texture *heartSymbolTexture = nullptr;
+SDL_Texture *frameTexture = nullptr;
+vector <SDL_Rect> obstaclesClipRect;
+
+void clipObstacles();
+void clipGUI();
+
+void loadMedia(GameWindow *win){
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+    backgroundTextures[0]           = win->loadTexture("assets/images/roads/road_5.png");
+    backgroundTextures[1]           = win->loadTexture("assets/images/roads/road_6.png");
+    carTexture                      = win->loadTexture("assets/images/cars/car.png");
+    carInvisibleTexture             = win->loadTexture("assets/images/cars/car_invisible.png");
+    obstacleSpriteTexture           = win->loadTexture("assets/images/cars/cars.png");
+    obstacleCrashedSpriteTexture    = win->loadTexture("assets/images/cars/cars_crashed.png");
+    coinSprite                      = win->loadTexture("assets/images/coin.png");
+    goldenFontTexture               = win->loadTexture("assets/fonts/golden.png");
+    blueFontTexture                 = win->loadTexture("assets/fonts/blue.png");
+    whiteFontTexture                = win->loadTexture("assets/fonts/white.png");
+    metalFontTexture                = win->loadTexture("assets/fonts/metal.png");
+    plainWhiteFontTexture           = win->loadTexture("assets/fonts/plain_white.png");
+    plainBlackFontTexture           = win->loadTexture("assets/fonts/plain_black.png");
+    heartSymbolTexture              = win->loadTexture("assets/images/HUD/heart.png");
+    frameTexture                    = win->loadTexture("assets/images/HUD/frame2.png");
+    clipObstacles();
+}
+
+void clipObstacles(){
+    obstaclesClipRect.push_back({253, 9, 49, 92});
+    obstaclesClipRect.push_back({314, 9, 49, 92});
+    obstaclesClipRect.push_back({373, 9, 49, 92});
+    obstaclesClipRect.push_back({71, 109, 50, 98});
+    obstaclesClipRect.push_back({133, 109, 50, 98});
+    obstaclesClipRect.push_back({195, 109, 50, 98});
+    obstaclesClipRect.push_back({15, 335, 46, 87});
+    obstaclesClipRect.push_back({69, 335, 46, 87});
+    obstaclesClipRect.push_back({127, 335, 46, 87});
+    obstaclesClipRect.push_back({183, 335, 46, 87});
+    obstaclesClipRect.push_back({12, 435, 50, 95});
+    obstaclesClipRect.push_back({78, 435, 50, 95});
+    obstaclesClipRect.push_back({143, 435, 50, 95});
+    obstaclesClipRect.push_back({206, 435, 50, 95});
+}
+
+void destroyTextures(){
+    SDL_DestroyTexture(backgroundTextures[0]);
+    SDL_DestroyTexture(backgroundTextures[1]);
+    SDL_DestroyTexture(carTexture);
+    SDL_DestroyTexture(carInvisibleTexture);
+    SDL_DestroyTexture(obstacleSpriteTexture);
+    SDL_DestroyTexture(obstacleCrashedSpriteTexture);
+    SDL_DestroyTexture(coinSprite);
+    SDL_DestroyTexture(goldenFontTexture);
+    SDL_DestroyTexture(blueFontTexture);
+    SDL_DestroyTexture(whiteFontTexture);
+    SDL_DestroyTexture(metalFontTexture);
+    SDL_DestroyTexture(plainWhiteFontTexture);
+    SDL_DestroyTexture(plainBlackFontTexture);
+    SDL_DestroyTexture(heartSymbolTexture);
+    SDL_DestroyTexture(frameTexture);
+}
---
+++ main/assets.hpp
@@ -0,0 +1,26 @@
+#ifndef ASSETS_HPP
+#define ASSETS_HPP
+
+#include "gamewindow.hpp"
+using namespace std;
+
+extern vector <SDL_Texture*> backgroundTextures;
+extern SDL_Texture *carTexture;
+extern SDL_Texture *carInvisibleTexture;
+extern SDL_Texture *obstacleSpriteTexture;
+extern SDL_Texture *obstacleCrashedSpriteTexture;
+extern SDL_Texture *coinSprite;
+extern SDL_Texture *goldenFontTexture;
+extern SDL_Texture *blueFontTexture;
+extern SDL_Texture *whiteFontTexture;
+extern SDL_Texture *metalFontTexture;
+extern SDL_Texture *plainWhiteFontTexture;
+extern SDL_Texture *plainBlackFontTexture;
+extern SDL_Texture *heartSymbolTexture;
+extern SDL_Texture *frameTexture;
+extern vector <SDL_Rect> obstaclesClipRect;
+
+void loadMedia(GameWindow *win);
+void destroyTextures();
+
+#endif // ASSETS_HPP
