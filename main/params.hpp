15f7952860f130c8085b4f441ffe6f078227cc7f
MagicianFrog <khang.na2005@gmail.com>
2024-05-05 23:30:10 +0700

    Constant parameters

---
+++ main/params.hpp
@@ -0,0 +1,21 @@
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+
+#define GAME_TITLE "PICK & DODGE"
+
+const double MOVEMENT_DELAY = 0.15;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 600;
+const int ROADSIDE_WIDTH = 90;
+const int CAR_WIDTH = 50;
+const int CAR_HEIGHT = 100;
+const int INIT_VELOCITY = 6 * 60;
+const int OBSTACLE_WIDTH = 50;
+const int OBSTACLE_HEIGHT = 100;
+const int NUMBER_OF_COLUMNS = 4;
+const int COIN_WIDTH = 30;
+const int COIN_HEIGHT = 30;
+const double NUMBER_OF_LIVES = 2;
+const int SPEED_BOOST_AMOUNT = 3 * 60;
+
+#endif // PARAMS_HPP
