d4bedf87f6cfb917062165ebcdd0a12c2012831c
MagicianFrog <khang.na2005@gmail.com>
2024-05-09 13:06:33 +0700

    Update movement delay for player's car

--- main/params.hpp
+++ main/params.hpp
@@ -3,7 +3,7 @@
 
 #define GAME_TITLE "PICK & DODGE"
 
-const double MOVEMENT_DELAY = 0.15;
+const double MOVEMENT_DELAY = 0.25;
 const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 600;
 const int ROADSIDE_WIDTH = 90;
@@ -15,7 +15,7 @@ const int OBSTACLE_HEIGHT = 100;
 const int NUMBER_OF_COLUMNS = 4;
 const int COIN_WIDTH = 30;
 const int COIN_HEIGHT = 30;
-const double NUMBER_OF_LIVES = 2;
+const double NUMBER_OF_LIVES = 5;
 const int SPEED_BOOST_AMOUNT = 3 * 60;
 
 #endif // PARAMS_HPP
