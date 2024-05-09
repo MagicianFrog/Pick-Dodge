b53c5e7ed2d509747e6b63669f9b78415822725b
MagicianFrog <khang.na2005@gmail.com>
2024-05-09 13:07:48 +0700

    Update movement delay for car

--- main/params.hpp
+++ main/params.hpp
@@ -16,6 +16,5 @@ const int NUMBER_OF_COLUMNS = 4;
 const int COIN_WIDTH = 30;
 const int COIN_HEIGHT = 30;
 const double NUMBER_OF_LIVES = 5;
-const int SPEED_BOOST_AMOUNT = 3 * 60;
 
 #endif // PARAMS_HPP
