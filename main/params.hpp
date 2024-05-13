9a170728ce4c47ae6a862bb47a826ce72b0230f4
MagicianFrog <khang.na2005@gmail.com>
2024-05-14 00:30:57 +0700

    Update several parameters

--- main/params.hpp
+++ main/params.hpp
@@ -4,9 +4,9 @@
 #define GAME_TITLE "PICK & DODGE"
 
 const double MOVEMENT_DELAY = 0.25;
-const int SCREEN_WIDTH = 500;
+const int SCREEN_WIDTH = 900;
 const int SCREEN_HEIGHT = 600;
-const int ROADSIDE_WIDTH = 90;
+const int ROADSIDE_WIDTH = 290;
 const int CAR_WIDTH = 50;
 const int CAR_HEIGHT = 100;
 const int INIT_VELOCITY = 6 * 60;
@@ -15,6 +15,6 @@ const int OBSTACLE_HEIGHT = 100;
 const int NUMBER_OF_COLUMNS = 4;
 const int COIN_WIDTH = 30;
 const int COIN_HEIGHT = 30;
-const double NUMBER_OF_LIVES = 5;
+const int NUMBER_OF_LIVES = 5;
 
 #endif // PARAMS_HPP
